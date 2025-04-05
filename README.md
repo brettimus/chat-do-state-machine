95bddb6580f699bb7f281a352b761f4c3c6646cb
Brett Beutell <brbeut@gmail.com>
2025-04-05 09:19:28 -0700

    Update readme

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 # xstate + cloudflare agents-sdk + drizzle
 
+This is a modified version of the Cloudflare Agents SDK example project.
+
+Instead of using the `ChatAgent` class, I've tried to implement something more suitable to our use case by extending the base `Agent` class.
+
+Drizzle handles interfacing with the DO's sqlite database.
+
+- `src/server.ts` contains the main logic for the agent.
+- `src/client.tsx` contains the frontend code for talking to the agent.
+
 ## Setup
 
 you need an openai api key to run this project.
 
 ```bash
 cp .dev.vars.example .dev.vars
-# edit .dev.vars with your openai api key
+# edit .dev.vars and add your openai api key
 ```
 
-install deps and run the dev server
+install deps, generate migration files, and run the dev server
 
 ```bash
 pnpm i
@@ -19,4 +28,15 @@ pnpm dev
 
 ## Notes
 
+### State Machines
+
+XState is used in this example on both the frontend and backend.
+
+- the `xstate-prototypes` directory is just a copy-paste of the one from the `spectacular` repo
+- the frontend uses a simple state machine to handle certain control flow around components
+
+### Drizzle
+
 - the drizzle migrations are executed automagically when the durable object is created
+
+- there is a custom Vite plugin to handle importing of the sql files in the migrations folder - it probably needs a more thorough view
--- src/server.ts
+++ src/server.ts
@@ -1,102 +1,14 @@
-import { routeAgentRequest, type Schedule } from "agents";
-
-import { unstable_getSchedulePrompt } from "agents/schedule";
-
-import { AIChatAgent } from "agents/ai-chat-agent";
-import {
-  createDataStreamResponse,
-  generateId,
-  streamText,
-  type StreamTextOnFinishCallback,
-} from "ai";
-import { openai } from "@ai-sdk/openai";
-import { processToolCalls } from "./utils";
-import { tools, executions } from "./tools";
-import { AsyncLocalStorage } from "node:async_hooks";
+import { routeAgentRequest } from "agents";
 import { fiberplane } from "@fiberplane/agents";
 
-// import { env } from "cloudflare:workers";
-
-const model = openai("gpt-4o-2024-11-20");
-// Cloudflare AI Gateway
-// const openai = createOpenAI({
-//   apiKey: env.OPENAI_API_KEY,
-//   baseURL: env.GATEWAY_BASE_URL,
-// });
-
-// we use ALS to expose the agent context to the tools
-export const agentContext = new AsyncLocalStorage<Chat>();
-
+/** Re-export the FpChatAgent Durable Object */
 export { FpChatAgent } from "./agents-server";
-
-/**
- * Chat Agent implementation that handles real-time AI chat interactions
- */
-export class Chat extends AIChatAgent<Env> {
-  /**
-   * Handles incoming chat messages and manages the response stream
-   * @param onFinish - Callback function executed when streaming completes
-   */
-
-  // biome-ignore lint/complexity/noBannedTypes: <explanation>
-  async onChatMessage(onFinish: StreamTextOnFinishCallback<{}>) {
-    // Create a streaming response that handles both text and tool outputs
-    return agentContext.run(this, async () => {
-      const dataStreamResponse = createDataStreamResponse({
-        execute: async (dataStream) => {
-          // Process any pending tool calls from previous messages
-          // This handles human-in-the-loop confirmations for tools
-          const processedMessages = await processToolCalls({
-            messages: this.messages,
-            dataStream,
-            tools,
-            executions,
-          });
-
-          // Stream the AI response using GPT-4
-          const result = streamText({
-            model,
-            system: `You are a helpful assistant that can do various tasks... 
-
-${unstable_getSchedulePrompt({ date: new Date() })}
-
-If the user asks to schedule a task, use the schedule tool to schedule the task.
-`,
-            messages: processedMessages,
-            tools,
-            onFinish,
-            onError: (error) => {
-              console.error("Error while streaming:", error);
-            },
-            maxSteps: 10,
-          });
-
-          // Merge the AI response stream with tool execution outputs
-          result.mergeIntoDataStream(dataStream);
-        },
-      });
-
-      return dataStreamResponse;
-    });
-  }
-  async executeTask(description: string, task: Schedule<string>) {
-    await this.saveMessages([
-      ...this.messages,
-      {
-        id: generateId(),
-        role: "user",
-        content: `Running scheduled task: ${description}`,
-        createdAt: new Date(),
-      },
-    ]);
-  }
-}
 
 /**
  * Worker entry point that routes incoming requests to the appropriate handler
  */
 export default {
-  // @ts-ignore - huh?
+  // @ts-ignore - unsure why this is happening
   fetch: fiberplane<Env>(
     async (request: Request, env: Env, ctx: ExecutionContext) => {
       if (!process.env.OPENAI_API_KEY) {
